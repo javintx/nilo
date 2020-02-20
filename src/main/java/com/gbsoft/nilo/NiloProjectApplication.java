301e98ae813040a1f66daf956ca79fa367d4785d
javint@hotmail.com <(J4v13r)>
2020-02-20 19:37:19 +0100

    First app

---
+++ src/main/java/com/gbsoft/nilo/NiloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gbsoft.nilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiloProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbsoft/nilo/NiloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbsoft.nilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiloProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
